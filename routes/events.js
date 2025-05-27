8654b3d36b586cf9ad05cd67a98f60799a67e6d4
Mahmoud Atie <maat0001@stud.kea.dk>
2025-05-27 10:08:11 +0200

    oprette en router med express.Router().

--- routes/events.js
+++ routes/events.js
@@ -11,4 +11,17 @@ router.put("/:id/book", eventsController.bookTickets);
 router.post("/reset", eventsController.resetEvents);
 router.post("/delete-image", eventsController.deleteImage);
 
-module.exports = router;
+// module.exports = router;
+
+// router.get("/", eventsController.getEvents);
+// router.post("/", eventsController.createEvent);
+// router.get("/:id", eventsController.getEventById);
+// router.patch("/:id", eventsController.updateEvent);
+// router.delete("/:id", eventsController.deleteEvent);
+// router.put("/:id/book", eventsController.bookTickets);
+// router.post("/reset", eventsController.resetEvents);
+
+// // 👇 tilføj denne nye route
+// router.post("/delete-image", eventsController.deleteImage);
+
+// module.exports = router;
